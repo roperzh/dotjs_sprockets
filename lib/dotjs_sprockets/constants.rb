98038689c7274605fe0795ccce6d7c0ebf8a0d99
Roberto Dip <dip.jesusr@gmail.com>
2014-07-12 14:06:21 -0300

    Add the constants module

---
+++ lib/dotjs_sprockets/constants.rb
@@ -0,0 +1,22 @@
+# Configuration constants, all of these are accessible from outside
+#
+# @since 0.1.0
+module DotjsSprockets
+  # Local gem assets
+  #
+  # Version 0.1.0 includes doT.js v1.0.1
+  ASSETS_PATH = "../../../vendor/assets/javascripts/dotjs_sprockets"
+
+  # Extension for the templates, sprockets will find and precompile all files
+  # with DotjsSprockets::EXTENSION
+  #
+  # This can be changed from the outside:
+  #
+  # DotjsSprockets::EXTENSION = ".dotjs"
+  #
+  # @since 0.1.0
+  EXTENSION   = ".djs"
+
+  # Current version
+  VERSION     = "0.1.0"
+end
